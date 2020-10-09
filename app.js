7fff0527088df42bf9a5298b076044995d7227a2
michaelfswann <mikeswann@mail.com>
2020-10-09 15:03:32 +0100

    populateTable function not working

--- app.js
+++ app.js
@@ -21,15 +21,39 @@ const pool = new Pool({
   uri: process.env.uri,
   ssl: {
     rejectUnauthorized : false }
- })
-  module.exports = {
-      query: (text, params, callback) => {
-      return pool.query(text, params, callback)
-  },
+})
+  
+// create table function
+
+async function createTable() {
+
+  let queryText = "CREATE TABLE my_recipes4 (id SERIAL PRIMARY KEY, title TEXT, ingredients TEXT, instructions TEXT, image TEXT)";
+ 
+  let res = await pool.query(queryText);
+
+  console.log(res);
+
 }
+
+createTable()
+
+// populate table function
+const populateTable = async () => {
+  for (const { id, title, ingredients, instructions, image } of data) {
+    const res = await pool.query(
+      `
+      INSERT INTO my_recipes4 (id SERIAL PRIMARY KEY, title TEXT, ingredients TEXT, instructions TEXT, image TEXT)
+        VALUES ($1, $2, $3, $4, $5)
+      `,
+      [id, title, ingredients, instructions, image] 
+    );
+    console.log(res);
+  }
+};
 
 const data = [
   {
+    id: "1",
     title: "Beans on Toast",
     ingredients: ["150g of beans", "150g of butter", "150g of toast"],
     instructions: `Put the butter in your mouth, wait 2 seconds to allow slight melting. Then follow with the toast. Swish around for 10-15 seconds to allow even coating of butter on the toast. Then add the beans, slowly.
@@ -40,12 +64,33 @@ const data = [
   }
 ];
 
+
+populateTable(data);
+
+
+
 //line 44-53 all requred for task 1.
 // get: tells servers what to do with http request 
 app.get("/", function (req, res) {
   res.sendFile(path.join(`${__dirname}/views/index.html`));
   console.log("get request")
 });
+
+
+app.get("/api/recipes", function (req, res) {
+  res.json({ payload : data });
+  console.log("get request")
+});
+
+app.post("/api/recipes", function (req, res) {
+  data.push(req.body);
+  res.json({ 
+    message: "post request success",
+     payload: req.body
+    });
+
+});
+
 //listen : tells server on port 500 for these requests
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
